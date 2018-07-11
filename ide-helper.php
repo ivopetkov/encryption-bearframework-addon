93454cae6c0b6e97ffc89468ee4c508463faad20
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 13:26:36 +0300

    Updated comments.

--- ide-helper.php
+++ ide-helper.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Encryption addon for Bear Framework
+ * https://github.com/ivopetkov/encryption-bearframework-addon
+ * Copyright (c) Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
 namespace BearFramework {
 
     /**

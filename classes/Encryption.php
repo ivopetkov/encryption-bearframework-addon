4790d81a155bed58cc1c52e630ced8cfa340281f
Ivo Petkov <ivo@ivopetkov.com>
2024-07-14 00:34:03 +0300

    Add keyPair methods. Update used cypher.

--- classes/Encryption.php
+++ classes/Encryption.php
@@ -18,12 +18,6 @@ class Encryption
 {
 
     /**
-     * The cipher method
-     * @var string 
-     */
-    private $cipher = 'AES-256-CBC';
-
-    /**
      *
      * @var array 
      */
@@ -48,8 +42,11 @@ class Encryption
         if ($key === null) {
             $key = $this->getDefaultKey();
         }
-        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->cipher));
-        return $iv . openssl_encrypt($value, $this->cipher, $key, 0, $iv);
+        $cypher = 'AES-256-GCM';
+        $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cypher));
+        $tag = null;
+        $content = openssl_encrypt($value, $cypher, $key, 0, $iv, $tag);
+        return json_encode([1, base64_encode($iv), base64_encode($tag)]) . $content; // first value is schema version
     }
 
     /**
@@ -57,16 +54,35 @@ class Encryption
      * 
      * @param string $value
      * @param string $key
-     * @return string
+     * @return string|null
      */
     public function decrypt(string $value, string $key = null): ?string
     {
         if ($key === null) {
             $key = $this->getDefaultKey();
         }
-        $ivLength = openssl_cipher_iv_length($this->cipher);
-        $iv = substr($value, 0, $ivLength);
-        $result = @openssl_decrypt(substr($value, $ivLength), $this->cipher, $key, 0, $iv);
+        if (substr($value, 0, 1) === '[') {
+            $separatorIndex = strpos($value, ']');
+            if ($separatorIndex === false) {
+                return null;
+            }
+            $config = json_decode(substr($value, 0, $separatorIndex + 1));
+            if ($config[0] === 1) { // schema version 1
+                $cypher = 'AES-256-GCM';
+                $iv = base64_decode($config[1]);
+                $tag = base64_decode($config[2]);
+            } else {
+                return null;
+            }
+            $content = substr($value, $separatorIndex + 1);
+        } else { // old format
+            $cypher = 'AES-256-CBC';
+            $ivLength = openssl_cipher_iv_length($cypher);
+            $iv = substr($value, 0, $ivLength);
+            $tag = null;
+            $content = substr($value, $ivLength);
+        }
+        $result = @openssl_decrypt($content, $cypher, $key, 0, $iv, $tag);
         if ($result === false) {
             return null;
         }
@@ -74,9 +90,51 @@ class Encryption
     }
 
     /**
+     * Encrypts the value provided with the public key specified.
+     * 
+     * @param string $value
+     * @param string $publicKey
+     * @return string
+     */
+    public function encryptWithPublicKey(string $value, string $publicKey): string
+    {
+        $key = $this->generateKey(rand(100, 200));
+        $encryptedKey = '';
+        $result = openssl_public_encrypt($key, $encryptedKey, $publicKey, OPENSSL_PKCS1_PADDING);
+        if ($result === false) {
+            throw new \Exception('Cannot encrypt key (' . strlen($key) . ')!');
+        }
+        return base64_encode($encryptedKey) . $this->encrypt($value, $key);
+    }
+
+    /**
+     * Decrypts the value provided with the private key specified.
+     * 
+     * @param string $value
+     * @param string $publicKey
+     * @return string|null
+     */
+    public function decryptWithPrivateKey(string $value, string $privateKey): ?string
+    {
+        $separatorIndex = strpos($value, '[');
+        if ($separatorIndex === false) {
+            return null;
+        }
+        $encryptedKey = base64_decode(substr($value, 0, $separatorIndex));
+        $encryptedContent = substr($value, $separatorIndex);
+
+        $decryptedKey = null;
+        $result = openssl_private_decrypt($encryptedKey, $decryptedKey, $privateKey);
+        if ($result === false) {
+            return null;
+        }
+        return $this->decrypt($encryptedContent, $decryptedKey);
+    }
+
+    /**
      * Returns the default key.
      */
-    private function getDefaultKey()
+    public function getDefaultKey()
     {
         if (isset($this->cache['defaultKey'])) {
             return $this->cache['defaultKey'];
@@ -88,12 +146,56 @@ class Encryption
             $dataKey = 'encryption/default.key';
             $value = $app->data->getValue($dataKey);
             if ($value === null) {
-                $value = md5(openssl_random_pseudo_bytes(openssl_cipher_iv_length($this->cipher)) . uniqid('', true) . rand(0, 999999999));
+                $value = $this->generateString(rand(32, 64));
                 $app->data->set($app->data->make($dataKey, $value));
             }
             $app->cache->set($app->cache->make($cacheKey, $value));
         }
         $this->cache['defaultKey'] = $value;
         return $value;
+    }
+
+    /**
+     * Generates a new key.
+     * 
+     * @param int $length
+     * @return string
+     */
+    public function generateKey(int $length): string
+    {
+        return $this->generateString($length);
+    }
+
+    /**
+     * Generates a new key pair.
+     * 
+     * @return array Returns [$privateKey, $publicKey]
+     */
+    public function generateKeyPair(): array
+    {
+        $key = openssl_pkey_new(["digest_alg" => "sha512", "private_key_bits" => 2048, "private_key_type" => OPENSSL_KEYTYPE_RSA]);
+        if ($key === false) {
+            throw new \Exception('Cannot generate key pair (' . openssl_error_string() . ')!');
+        }
+        $privateKey = '';
+        openssl_pkey_export($key, $privateKey);
+        $publicKey = openssl_pkey_get_details($key)['key'];
+        return [$privateKey, $publicKey];
+    }
+
+    /**
+     * Generates a random string.
+     * 
+     * @param integer $length
+     * @return string
+     */
+    private function generateString(int $length): string
+    {
+        $result = '';
+        $letters = str_split('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890', 1);
+        for ($i = 0; $i < $length; $i++) {
+            $result .= $letters[array_rand($letters, 1)];
+        }
+        return $result;
     }
 }
--- tests/EncryptionTest.php
+++ tests/EncryptionTest.php
@@ -16,11 +16,36 @@ class EncryptionTest extends BearFramework\AddonTests\PHPUnitTestCase
     /**
      * 
      */
-    public function testEncrypt()
+    public function testEncryptDecrypt()
     {
         $app = $this->getApp();
-        $value = $app->encryption->encrypt('123');
-        $this->assertTrue($app->encryption->decrypt($value) === '123');
+        $text = $this->generateString(10000000);
+        $value = $app->encryption->encrypt($text);
+        $this->assertTrue($app->encryption->decrypt($value) === $text);
+    }
+
+    /**
+     * 
+     */
+    public function testOldFormatEncryptDecrypt()
+    {
+        $app = $this->getApp();
+        $text = 'UG4VNRiVGVSa0Saf1flTEFqQbZnL8rNjSQGLQWWrTH6zeKOuLQuTq2wiA46S9fJrUgIHRMu6uSEU1Rqg6eilSY7K5llijOoSTuQC';
+        $key = 'secret1';
+        $encryptedText = '1QXXy6VDiFQbIjdrligWeE5zcWd0bFFLR3hjTnRyekIrejdKQmI2N1pLbEIrNjZhZnJLNXpaaCtJbkhGSUU1bTA3ZVRGdWtHT3BWRDVDQlcraFBYMlBFUXJPUnFHM284WFFhSHdCT0Vxc3RmS09xbzMwSFJNbjhmU2NPRFdzQitBZkRwVkh5ZENTTDBpeTkwS21ReEo3QVN5Z0xkT0hrRFlmZ3d4dz09'; // base64 encoded
+        $this->assertTrue($app->encryption->decrypt(base64_decode($encryptedText), $key) === $text);
+    }
+
+    /**
+     * 
+     */
+    public function testKeyPairEncryptDecrypt()
+    {
+        $app = $this->getApp();
+        $text = $this->generateString(10000000);
+        list($privateKey, $publicKey) = $app->encryption->generateKeyPair();
+        $value = $app->encryption->encryptWithPublicKey($text, $publicKey);
+        $this->assertTrue($app->encryption->decryptWithPrivateKey($value, $privateKey) === $text);
     }
 
     /**
@@ -30,6 +55,21 @@ class EncryptionTest extends BearFramework\AddonTests\PHPUnitTestCase
     {
         $app = $this->getApp();
         $this->assertTrue($app->encryption->decrypt('123') === null);
+        $this->assertTrue($app->encryption->decryptWithPrivateKey('123', 'key') === null);
     }
 
+    /**
+     * 
+     * @param integer $length
+     * @return string
+     */
+    private function generateString(int $length): string
+    {
+        $result = '';
+        $letters = str_split('qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890', 1);
+        for ($i = 0; $i < $length; $i++) {
+            $result .= $letters[array_rand($letters, 1)];
+        }
+        return $result;
+    }
 }
